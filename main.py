862a378600abe8214263c3eeb2e434d53c61c980
Henrique Cecconi <henrique.cecconi@gmail.com>
2022-04-28 19:30:02 -0300

    Slicing the url to separate the base from the parameters.

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-url = "http://bytebank.com/exchange?inputCurrency=real&outputCurrency=dollar&quantity=100"
+url = 'http://bytebank.com/exchange?inputCurrency=real&outputCurrency=dollar&quantity=100'
+
+question_index = url.find('?')
+url_base = url[:question_index]
+url_parameters = url[question_index+1:]
