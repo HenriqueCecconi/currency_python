8d11fc934ad37274fdeaa9a5974b4a3f208eb129
Henrique Cecconi <henrique.cecconi@gmail.com>
2022-04-28 19:59:32 -0300

    Adding a basic logic to find a single parameter within the URL, being it in the middle or in the end of the string. This is a sketch, will add logic to find all parameters later.

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 url = 'http://bytebank.com/exchange?inputCurrency=real&outputCurrency=dollar&quantity=100'
 
+# Separate base and parameters
 question_index = url.find('?')
 url_base = url[:question_index]
 url_parameters = url[question_index+1:]
+
+parameter_name = 'outputCurrency'
+parameter_index = url_parameters.find(parameter_name)
+value_index = parameter_index + len(parameter_name) + 1
+ampersand_index = url_parameters.find('&', value_index)
+if ampersand_index == 1:
+    parameter_value = url[value_index:]
+else:
+    parameter_value = url[value_index:ampersand_index]
