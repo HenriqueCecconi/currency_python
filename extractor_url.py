b912de78979984e20dd78dcc5d8176a9d9e8db1c
Henrique Cecconi <henrique.cecconi@gmail.com>
2022-04-30 12:32:15 -0300

    Applying OOP concepts to the code. Making ExtractorURL into a class, and making methods to validate, sanitize, extract the base and the parameters of the url, and finding the value of a parameter

---
+++ extractor_url.py
@@ -0,0 +1,34 @@
+class ExtractorURL():
+    def __init__(self, url):
+        self.url = self.sanitize_url(url)
+        self.validate_url()
+    
+    def sanitize_url(self, url):
+        return url.strip()
+
+    def validate_url(self):
+        if self.url == '':
+            raise ValueError('The URL is empty')
+        else:
+            return url
+            
+    def get_url_base(self):
+        question_index = self.url.find('?')
+        url_base = self.url[:question_index]
+        return url_base 
+
+    def get_url_parameter(self):
+        question_index = self.url.find('?')
+        url_parameter = self.url[question_index+1:]
+        return url_parameter
+        
+    def get_parameter_value(self, parameter_name):
+        parameter_index = self.get_url_parameter().find(parameter_name)
+        value_index = parameter_index + len(parameter_name) + 1
+        ampersand_index = self.get_url_parameter().find('&', value_index)
+        if ampersand_index == 1:
+            parameter_value = self.get_url_parameter()[value_index:]
+        else:
+            parameter_value = self.get_url_parameter()[value_index:ampersand_index]
+
+        return parameter_value
